e38f13a264f827bd265918f50c92df0d9352a6c6
chriscauley <chris@lablackey.com>
2016-07-17 16:47:26 -0500

    f1rst

---
+++ node_helper.js
@@ -0,0 +1,23 @@
+var NodeHelper = require("node_helper");
+var api = require('instagram-node').instagram();
+
+module.exports = NodeHelper.create({
+	// Subclass start method.
+	start: function() {
+	},
+
+	// Subclass socketNotificationReceived received.
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "ADD_FEED") {
+		  this.client = api.use(payload);
+      var self = this;
+      this.client.user_self_feed(function(err, medias, pagination, remaining, limit) {
+        if (!error) {
+          self.sendSocketNotification("NEW_GRAMS", medias);
+        } else { console.error(error) }
+      });
+			
+			return;
+		}
+	},
+});
